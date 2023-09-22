31dd8d94ba9495183913595ef5ac7145a7570809
suttha jongjaroen <6410615121@cn330>
2023-09-22 11:08:28 +0700

    change quota app to course

--- course/apps.py
+++ course/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class QuotaConfig(AppConfig):
+class CourseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'quota'
+    name = 'course'
--- reg/settings.py
+++ reg/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quota',
+    'course',
     'users',
 ]
 
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bangkok'
 
 USE_I18N = True
 
