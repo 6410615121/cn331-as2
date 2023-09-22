00b77d866103aab81e039bc65a24bfc34ccf5038
suttha jongjaroen <6410615121@cn330>
2023-09-22 12:48:36 +0700

    change name quotaAccept to quotaAccepted

--- users/models.py
+++ users/models.py
@@ -6,8 +6,8 @@ class Student(models.Model):
     name = models.CharField(max_length=50)
     id = models.CharField(max_length=10,primary_key=True)
 
-    quotaRequest = models.ManyToManyField("course.Course", related_name='quotaRequest')
-    quotaAccept = models.ManyToManyField("course.Course", related_name='quotaAccept')
+    quotaRequested = models.ManyToManyField("course.Course", related_name='quotaRequest')
+    quotaAccepted = models.ManyToManyField("course.Course", related_name='quotaAccepted')
     
     def __str__(self):
         return self.id + ' ' + self.name
