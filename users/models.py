abe335061dbea0d1ce44ca3c15a3dcd6ac0a451a
suttha jongjaroen <6410615121@cn330>
2023-09-22 12:03:14 +0700

    add Student

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    id = models.CharField(max_length=10,primary_key=True)
+
+    def __str__(self):
+        return self.id + ' ' + self.name
+
